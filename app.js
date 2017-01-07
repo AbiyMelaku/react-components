b0a98e2beff3427bdd54eb9f2dcc25c96818ffd4
AbiyMelaku <agirma08@gmail.com>
2017-01-06 16:14:21 -0800

    completed react components mini-sprint

--- app.js
+++ app.js
@@ -1,1 +1,51 @@
-// TODO
+
+
+
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hover : false
+    };
+  }
+
+  render() {
+    var style = {
+      fontWeight: this.state.hover ? 'bold' : 'normal'
+    };
+
+
+    return (
+      <li style={style} onMouseEnter={this.updateHover.bind(this)} onMouseLeave={this.updateHover.bind(this)}>{this.props.item}</li>
+    )
+  }
+
+  updateHover() {
+    this.setState({
+      hover: !this.state.hover
+    });
+    console.log(this.state.hover)
+  }
+
+}
+  
+
+var GroceryList = (props) => {
+  console.log(props);
+  return (<div>
+    <h2> Grocery List </h2>
+    {props.items.map((item) => (<GroceryListItem item={item}/>))}
+  </div>)
+};
+
+var groceryItems = ['milk', 'cookies'];
+
+//click events
+var onListItemClick = (event) => {
+  console.log('I got clicked');
+};
+
+
+ReactDOM.render(<GroceryList items={groceryItems} />, document.getElementById("app"));
